b503e450cc106e73a1fc1ea08a5ee8458f6a8164
Julian <justdotJS@icloud.com>
2020-01-30 20:25:36 +0800

    Initial Commit

---
+++ Coronacheck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coronacheck
+//
+//  Created by Julian Schiavo on 30/1/2020.
+//  Copyright © 2020 Julian Schiavo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
