a253e5a7dc7366226e00de0874ed8e7f0b6850b1
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-03-16 12:53:13 -0700

    Fix GitHub spelling

--- Covidcheck/InfoView.swift
+++ Covidcheck/InfoView.swift
@@ -21,7 +21,7 @@ struct InfoView: View {
         NavigationView {
             List {
                 Section(header: Text("ABOUT")) {
-                    Text("Covidcheck is made by Julian Schiavo and open sourced on Github under the Unlicense license.")
+                    Text("Covidcheck is made by Julian Schiavo and open sourced on GitHub under the Unlicense license.")
                 }
                 Section(header: Text("PREFERENCES")) {
                     ForEach(allPreferences, id: \.id) { preference in
